85021c28756f83b679bec723fd12a074a3530e6b
SekaiRed <sekairedblue@gmail.com>
2021-12-10 15:34:30 +0100

    Cleaned up but height fixer still broken

--- src/main/java/com/sekai/desirelines/Events.java
+++ src/main/java/com/sekai/desirelines/Events.java
@@ -6,24 +6,26 @@ import it.unimi.dsi.fastutil.ints.IntIterator;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class Events {
-    private static Int2ObjectMap<Vector3d> lastEntitiesPos = new Int2ObjectLinkedOpenHashMap<>();
+    private static final Int2ObjectMap<Vector3d> lastEntitiesPos = new Int2ObjectLinkedOpenHashMap<>();
 
-    private static void decay(Entity living) {
+    private static void decay(LivingEntity living) {
         World world = living.level;
 
         if (!world.isClientSide && canEntityDecay(living)) {
@@ -41,7 +43,7 @@ public class Events {
             for(Config.BlockDecayEntry entry : Config.blockDecayEntries) {
                 if(block.equals(ForgeRegistries.BLOCKS.getValue(entry.source))) {
                     if(random < (entry.getChance() * factor)) {
-                        decayBlock(world, living, pos, entry);
+                        decayBlock(living, state, pos, world, entry);
                         return;
                     }
                 }
@@ -50,57 +52,76 @@ public class Events {
 
     }
 
-    private static boolean canEntityDecay(Entity living) {
+    private static boolean canEntityDecay(LivingEntity living) {
         //System.out.println(living + " " + isEntityMoving(living) + " " + !living.isPassenger() + " " + !living.isShiftKeyDown());
-        return isEntityMoving(living) && !living.isPassenger() && !living.isShiftKeyDown() && !living.isSpectator();
+        return isEntityMoving(living) && !living.isPassenger() && !living.isShiftKeyDown() && !living.isSpectator() && living.isOnGround();
     }
 
-    private static void decayBlock(World world, Entity living, BlockPos entityLocation, Config.BlockDecayEntry entry) {
+    private static void decayBlock(LivingEntity living, BlockState originalState, BlockPos pos, World world, Config.BlockDecayEntry entry) {
         Block block = ForgeRegistries.BLOCKS.getValue(entry.result);
         if(block == null)
             return;
 
-        BlockState state = block.defaultBlockState();
-        double newHeight = entityLocation.getY() + state.getCollisionShape(world, entityLocation).max(Direction.Axis.Y);
+        BlockState newState = block.defaultBlockState();
+
+        VoxelShape shape = newState.getCollisionShape(world, pos).move((double)pos.getX(), (double)pos.getY() - 0.001D, (double)pos.getZ());
+
+        for(Entity entity : world.getEntities((Entity)null, shape.bounds())) {
+            double d0 = VoxelShapes.collide(Direction.Axis.Y, entity.getBoundingBox().move(0.0D, 1.0D, 0.0D), Stream.of(shape), -1.0D);
+            entity.teleportTo(entity.getX(), entity.getY() + 1.0D + d0, entity.getZ());
+        }
+
+        world.setBlockAndUpdate(pos, block.defaultBlockState());
+
+        /*newState.getCollisionShape(world, pos).bounds()
+
+        if(block.defaultBlockState().getCollisionShape(world, pos).max(Direction.Axis.Y) == originalState.getCollisionShape(world, pos).max(Direction.Axis.Y)) {
+            world.setBlockAndUpdate(pos, block.defaultBlockState());
+        } else {
+            world.setBlockAndUpdate(pos, Block.pushEntitiesUp(originalState, block.defaultBlockState(), world, pos));
+        }*/
+
+        /*System.out.println(originalState.getCollisionShape(world, pos) + " " + block.defaultBlockState().getCollisionShape(world, pos));
+
+        world.setBlockAndUpdate(pos, Block.pushEntitiesUp(originalState, block.defaultBlockState(), world, pos));*/
+
+        /*BlockState state = block.defaultBlockState();
+        double newHeight = pos.getY() + state.getCollisionShape(world, pos).max(Direction.Axis.Y);
 
         //TODO check for all living entities
         if(newHeight > living.getY())
             living.setPos(living.getX(), newHeight, living.getZ());
 
-        world.setBlock(entityLocation, state, 3);
+        world.setBlock(pos, state, 3);*/
     }
 
     //TODO easier calculation
-    private static boolean isEntityMoving(Entity living) {
+    private static boolean isEntityMoving(LivingEntity living) {
         if(lastEntitiesPos.keySet().contains(living.getId())) {
-            boolean isMoving = false;
-
             Vector3d lastPos = lastEntitiesPos.getOrDefault(living.getId(), living.position());
-
-            if (Math.abs(living.getX() - lastPos.x()) > 0.0D || Math.abs(living.getY() - lastPos.y()) > 0.0D || Math.abs(living.getZ() - lastPos.z()) > 0.0D)
-                isMoving = true;
 
             //update position
             lastEntitiesPos.put(living.getId(), living.position());
 
-            return isMoving;
+            return Math.abs(living.getX() - lastPos.x()) > 0.0D || Math.abs(living.getY() - lastPos.y()) > 0.0D || Math.abs(living.getZ() - lastPos.z()) > 0.0D;
         } else {
             lastEntitiesPos.put(living.getId(), living.position());
             return false;
         }
+    }
 
-        //boolean isMoving = false;
+    private static double getEntityMoveSpeed(LivingEntity living) {
+        if(lastEntitiesPos.keySet().contains(living.getId())) {
+            Vector3d lastPos = lastEntitiesPos.getOrDefault(living.getId(), living.position());
 
-        /*if(living instanceof PlayerEntity) {
-            System.out.println(" 1) " + living.position().subtract(new Vector3d(living.xOld, living.yOld, living.zOld)));
-            System.out.println(" 2) " + living.position().subtract(new Vector3d(living.xo, living.yo, living.zo)));
-            System.out.println(" 3) " + living.getDeltaMovement());
-        }*/
+            //update position
+            lastEntitiesPos.put(living.getId(), living.position());
 
-        /*if (Math.abs(living.getX() - living.xOld) > 0.0D || Math.abs(living.getY() - living.yOld) > 0.0D || Math.abs(living.getZ() - living.zOld) > 0.0D)
-            isMoving = true;
-
-        return isMoving;*/
+            return living.position().subtract(lastPos).length();
+        } else {
+            lastEntitiesPos.put(living.getId(), living.position());
+            return 0D;
+        }
     }
 
     @SubscribeEvent
@@ -118,68 +139,28 @@ public class Events {
     public static void checkForErasedMobs(TickEvent.WorldTickEvent event) {
         if(event.side.isServer() && event.phase.equals(TickEvent.Phase.END)) {
             if(event.world instanceof ServerWorld) {
+                //long time = System.nanoTime();
                 ServerWorld serverWorld = ((ServerWorld) event.world);
                 IntIterator iterator = lastEntitiesPos.keySet().iterator();
-                int j = 0;
+                boolean fakeServer = false;
                 while(iterator.hasNext()) {
-                    j++;
                     int key = iterator.nextInt();
-                    //The key doesn't exist anymore, rip, remove nao
-                    /*if(!serverWorld.getEntities().allMatch(entity -> {
-                        //System.out.println(entity.getId() + " comp " + key + " returns " + (entity.getId() != key));
-                        return entity.getId() != key;
-                    })) {*/
-                    /*if(serverWorld.getEntities().noneMatch(entity -> {
-                        System.out.println(entity.getId() + " comp " + key + " returns " + (entity.getId() == key));
-                        return entity.getId() == key;
-                    })) {
-                        System.out.println("removed " + key);
-                        iterator.remove();
-                    }*/
                     boolean match = false;
-                    //System.out.println(serverWorld.getAllEntities().spliterator().estimateSize());
                     int i = 0;
                     for(Entity entity : serverWorld.getAllEntities()) {
                         i++;
                         if(entity.getId() == key)
                             match = true;
-                        //System.out.println(entity.getId() + " == " + key + " is " + (entity.getId() == key) + " and match is " + match);
                     }
                     if(!match && i!=0) {
-                        //System.out.println("1) removed " + key + " with match at " + match + " " + i + " " + j);
                         iterator.remove();
                     }
-                    /*if(!match) {
-                        System.out.println("1) removed " + key + " with match at " + match + " " + i + " " + j);
-                        //iterator.remove();
-                    }
-                    if(match) {
-                        System.out.println("2) removed " + key + " with match at " + match + " " + i + " " + j);
-                        //iterator.remove();
-                    }*/
+                    if(i == 0)
+                        fakeServer = true;
                 }
+                /*if(!fakeServer)
+                    System.out.println(System.nanoTime() - time);*/
             }
-            /*if(event.world
-            //getAllEntities()
-            IntIterator iterator = lastEntitiesPos.keySet().iterator();
-            while(iterator.hasNext()) {
-                int key = iterator.nextInt();
-            }
-            System.out.println("fuck");*/
         }
     }
-
-    /*@SubscribeEvent
-    public static void decayMob(LivingEvent.LivingUpdateEvent event) {
-        if (event.getEntityLiving() != null && !(event.getEntityLiving() instanceof PlayerEntity) && Config.shouldMobDecay) {
-            decay(event.getEntityLiving());
-        }
-    }*/
-
-    /*@SubscribeEvent
-    public static void decayPlayer(PlayerEvent event) {
-        if (event.getPlayer() != null) {
-            decay(event.getPlayer());
-        }
-    }*/
 }
